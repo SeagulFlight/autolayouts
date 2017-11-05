d5cee15b02a5a85c6c973adb08a47e9ac3eda7e3
Dwayne Kavanagh <dwaynekavanagh@Dwaynes-MacBook-Air.local>
2017-11-05 07:29:08 -0500

    initial xcode project

---
+++ autolayouts/autolayoutsTests/autolayoutsTests.swift
@@ -0,0 +1,36 @@
+//
+//  autolayoutsTests.swift
+//  autolayoutsTests
+//
+//  Created by Dwayne Kavanagh on 2017-11-05.
+//  Copyright © 2017 Dwayne Kavanagh. All rights reserved.
+//
+
+import XCTest
+@testable import autolayouts
+
+class autolayoutsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
